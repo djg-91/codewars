0cdb3596b0a22d4dc3faf6b66506314ff5ad4d6a
djg-91 <jose.constenla@gmail.com>
2024-05-26 14:14:45 +0200

    feat: add kata "You Can't Code Under Pressure #1"

---
+++ 8 kyu/PHP/You Can't Code Under Pressure #1.php
@@ -0,0 +1,10 @@
+/*
+https://www.codewars.com/kata/53ee5429ba190077850011d4
+
+    Code as fast as you can! You need to double the integer and return it.
+*/
+
+
+function doubleInteger($i) {
+    return $i * 2;
+}
