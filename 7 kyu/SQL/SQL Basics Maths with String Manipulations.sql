6fde36c64c42cac5c9add673406d87e787afa6f7
djg-91 <jose.constenla@gmail.com>
2024-06-03 23:38:14 +0200

    feat: add kata 'SQL Basics Maths with String Manipulations'

---
+++ 7 kyu/SQL/SQL Basics Maths with String Manipulations.sql
@@ -0,0 +1,18 @@
+/*
+https://www.codewars.com/kata/594901ba44645fd7bd00005f
+
+    Given a demographics table in the following format:
+
+    demographics table schema
+        id
+        name
+        birthday
+        race
+
+    return a single column named calculation where the value is the bit length of name, 
+    added to the number of characters in race.
+*/
+
+
+SELECT BIT_LENGTH(name) + LENGTH(race) AS calculation
+FROM demographics;
