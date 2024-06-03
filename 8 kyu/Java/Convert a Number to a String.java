fd6e5e5a553328054ffcbad608686ba5f978bbf6
djg-91 <jose.constenla@gmail.com>
2024-06-03 23:31:43 +0200

    feat: add kata 'Convert a Number to a String'

---
+++ 8 kyu/Java/Convert a Number to a String.java
@@ -0,0 +1,19 @@
+/*
+https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+
+    We need a function that can transform a number (integer) into a string.
+
+    What ways of achieving this do you know?
+
+    Examples (input --> output):
+        123  --> "123"
+        999  --> "999"
+        -100 --> "-100"
+*/
+
+
+class Kata {
+  public static String numberToString(int num) {
+    return num + "";
+  }
+}
