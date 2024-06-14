29e972c09fcddb43106ccb807aa1a27ea15d07b7
djg-91 <jose.constenla@gmail.com>
2024-06-14 20:22:31 +0200

    feat: add kata 'Which are in'

---
+++ 6 kyu/Python/Which are in.py
@@ -0,0 +1,31 @@
+'''
+https://www.codewars.com/kata/550554fd08b86f84fe000a58
+
+    Given two arrays of strings a1 and a2 return a sorted array r in lexicographical 
+    order of the strings of a1 which are substrings of strings of a2.
+
+    Example 1:
+
+        a1 = ["arp", "live", "strong"]
+
+        a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+        returns ["arp", "live", "strong"]
+
+    Example 2:
+
+        a1 = ["tarp", "mice", "bull"]
+
+        a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+        returns []
+
+    Notes:
+        Arrays are written in "general" notation. See "Your Test Cases" for examples in your language.
+        In Shell bash a1 and a2 are strings. The return is a string where words are separated by commas.
+        Beware: In some languages r must be without duplicates.
+'''
+
+
+def in_array(array1, array2):
+    return sorted(list(set([x for x in array1 if x in ' '.join(array2)])))
