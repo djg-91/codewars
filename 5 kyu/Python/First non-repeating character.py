c88780cd172aa391e9e949f540c1112741c3e646
djg-91 <jose.constenla@gmail.com>
2024-07-04 12:03:18 +0200

    feat: add kata 'First non-repeating character'

---
+++ 5 kyu/Python/First non-repeating character.py
@@ -0,0 +1,25 @@
+'''
+https://www.codewars.com/kata/52bc74d4ac05d0945d00054e
+
+    Write a function named first_non_repeating_letter† that takes a string input, and returns 
+    the first character that is not repeated anywhere in the string.
+
+    For example, if given the input 'stress', the function should return 't', since the 
+    letter t only occurs once in the string, and occurs first in the string.
+
+    As an added challenge, upper- and lowercase letters are considered the same character, 
+    but the function should return the correct case for the initial letter. For example, 
+    the input 'sTreSS' should return 'T'.
+
+    If a string contains all repeating characters, it should return an empty string ("");
+
+    † Note: the function is called firstNonRepeatingLetter for historical reasons, but your 
+    function should handle any Unicode character.
+'''
+
+
+def first_non_repeating_letter(string):
+    for c in string:
+        if string.lower().count(c.lower()) == 1:
+            return c
+    return ''
